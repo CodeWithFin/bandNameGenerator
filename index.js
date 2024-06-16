7cae174c7f6f88e885290f30e368f13a8f17c2ce
Finley <finley.mwachia12@gmail.com>
2024-06-16 08:39:59 +0300

    random commit

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   //Step 1 - Make the get route work and render the index.ejs file.
-  res.render("index.ejs",{randomText});
+  res.render("index.ejs", { adjective: randomAdj,
+    noun: randomNoun,});
 });
 
 app.post("/submit", (req, res) => {
@@ -28,17 +29,16 @@ app.post("/submit", (req, res) => {
   //scroll down to see the two arrays.
   //2. Send the index.ejs as a response and add the adjective and noun to the res.render
   //3. Test to make sure that the random words display in the h1 element in index.ejs
-  var randomAdj = Math.floor(Math.random()*adj.length);
-  var randomNoun = Math.floor(Math.random()*noun.length);
-  
+  var randomAdj = Math.floor(Math.random() * adj.length);
+  var randomNoun = Math.floor(Math.random() * noun.length);
+
   randomText = `${adj[randomAdj]} ${noun[randomNoun]}`;
   console.log(randomText);
-  
-  
 });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
+  console.log(port)
 });
 
 const adj = [
